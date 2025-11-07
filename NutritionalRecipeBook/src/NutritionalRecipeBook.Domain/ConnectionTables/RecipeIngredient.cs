ba20b3453a8c080a8d45adeaf903f48793666217
shepelenko.m <mykhailo.shepelenko@nixs.com>
2025-11-07 17:48:38 +0100

    Update RecipeIngredient entity

--- NutritionalRecipeBook/src/NutritionalRecipeBook.Domain/ConnectionTables/RecipeIngredient.cs
+++ NutritionalRecipeBook/src/NutritionalRecipeBook.Domain/ConnectionTables/RecipeIngredient.cs
@@ -2,7 +2,7 @@ using NutritionalRecipeBook.Domain.Entities;
 
 namespace NutritionalRecipeBook.Domain.ConnectionTables;
 
-public class RecipeIngredient
+public class RecipeIngredient: BaseEntity<(Guid RecipeId, Guid IngredientId)>
 {
     public Guid RecipeId { get; set; }
     public virtual Recipe Recipe { get; set; } = null!;
