d052482f699264d7cbe7659f949ed093a6143d5c
shepelenko.m <mykhailo.shepelenko@nixs.com>
2025-11-07 14:10:50 +0100

    Update TailwindCSS

--- NutritionalRecipeBook.UI/vite.config.ts
+++ NutritionalRecipeBook.UI/vite.config.ts
@@ -1,13 +1,11 @@
 import react from '@vitejs/plugin-react-swc';
-import tailwindcss from '@tailwindcss/vite'
-
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), tailwindcss(),],
+  plugins: [react(), tsconfigPaths(), ],
   define: {
     'process.env': {},
   },
