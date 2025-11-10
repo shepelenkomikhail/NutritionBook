ac6f4da6223ac23ef378b8d31e56bbe54e069d78
shepelenko.m <mykhailo.shepelenko@nixs.com>
2025-11-10 15:42:18 +0100

    Add theme toggle

--- NutritionalRecipeBook.UI/src/components/recipe/Recipe.tsx
+++ NutritionalRecipeBook.UI/src/components/recipe/Recipe.tsx
@@ -1,13 +1,15 @@
-import { useState } from 'react';
-import { PlusOutlined } from '@ant-design/icons';
-import { Modal, FloatButton, Spin, Layout } from 'antd';
-import { RecipeForm } from './RecipeForm';
-import { useRecipeMutation } from '../../hooks';
+import { useContext, useState } from 'react';
+import { PlusOutlined, SunOutlined, MoonOutlined } from '@ant-design/icons';
 import { RecipeModel } from '@models';
-
-const { Content } = Layout;
+import { useRecipeMutation } from '../../hooks';
+import { RecipeForm } from './RecipeForm';
+import { FloatButton, Layout, Modal, Spin, Button } from 'antd';
+import Title from 'antd/es/typography/Title';
+import { ThemeContext } from '../../layout/App';
+const { Content, Header } = Layout;
 
 function Recipe() {
+  const { theme, setTheme } = useContext(ThemeContext);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const { execute, isLoading } = useRecipeMutation('create');
 
@@ -19,28 +21,44 @@ function Recipe() {
     setIsModalOpen(false);
   };
 
-  return (
-    <Content className="p-6 relative min-h-screen flex">
-      <FloatButton
-        icon={<PlusOutlined />}
-        type="primary"
-        tooltip={<div>Add Recipe</div>}
-        onClick={handleOpen}
-      />
+  const handleThemeToggle = () => {
+    setTheme(theme === 'dark' ? 'light' : 'dark');
+    console.log(theme);
+  }
 
-      <Modal
-        title="Create New Recipe"
-        open={isModalOpen}
-        onCancel={handleCancel}
-        destroyOnClose
-        footer={null}
-        className="max-h-[70vh] overflow-y-auto"
-      >
-        <Spin spinning={isLoading} tip="Processing...">
-          <RecipeForm mode="create" onSubmit={handleSubmit} isLoading={isLoading} />
-        </Spin>
-      </Modal>
-    </Content>
+  return (
+    <>
+      <Header className={"flex items-center justify-center w-full relative"}>
+        <Button
+          icon={theme === 'dark' ? <SunOutlined style={{ color: 'white', fontSize: '24px'}} /> : <MoonOutlined style={{ color: 'white', fontSize: '24px'}} />}
+          type="primary"
+          className="!absolute top-2 right-4 !h-12 !w-12"
+          onClick={handleThemeToggle}
+        />
+        <Title>Recipes</Title>
+      </Header>
+
+      <Content className="p-6 relative min-h-screen flex">
+        <FloatButton
+          icon={<PlusOutlined />}
+          type="primary"
+          tooltip={<div>Add Recipe</div>}
+          onClick={handleOpen}
+        />
+        <Modal
+          title="Create New Recipe"
+          open={isModalOpen}
+          onCancel={handleCancel}
+          destroyOnClose
+          footer={null}
+          className="max-h-[70vh] overflow-y-auto"
+        >
+          <Spin spinning={isLoading} tip="Processing...">
+            <RecipeForm mode="create" onSubmit={handleSubmit} isLoading={isLoading} />
+          </Spin>
+        </Modal>
+      </Content>
+    </>
   );
 }
 
--- NutritionalRecipeBook.UI/src/models/index.ts
+++ NutritionalRecipeBook.UI/src/models/index.ts
@@ -1,3 +1,4 @@
 export * from './RecipeModel';
 export * from './IngredientModel';
 export * from './RecipePayload';
+export * from './ThemeModel';
