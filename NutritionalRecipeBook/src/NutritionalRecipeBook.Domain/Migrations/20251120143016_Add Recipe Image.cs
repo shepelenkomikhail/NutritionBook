4821f0fd032e151996fffef8b0cdb6a96ddc1dff
shepelenko.m <mykhailo.shepelenko@nixs.com>
2025-11-20 15:31:36 +0100

    Add ImageUrl column to Recipes table and update related data

---
+++ NutritionalRecipeBook/src/NutritionalRecipeBook.Domain/Migrations/20251120143016_Add Recipe Image.cs
@@ -0,0 +1,381 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NutritionalRecipeBook.Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRecipeImage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Recipes",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "Comments",
+                keyColumn: "Id",
+                keyValue: new Guid("50000000-0000-0000-0000-000000000001"),
+                column: "CreatedAt",
+                value: new DateTime(2025, 11, 20, 14, 20, 15, 517, DateTimeKind.Utc).AddTicks(6855));
+
+            migrationBuilder.UpdateData(
+                table: "Comments",
+                keyColumn: "Id",
+                keyValue: new Guid("50000000-0000-0000-0000-000000000002"),
+                column: "CreatedAt",
+                value: new DateTime(2025, 11, 20, 14, 10, 15, 517, DateTimeKind.Utc).AddTicks(6872));
+
+            migrationBuilder.UpdateData(
+                table: "Comments",
+                keyColumn: "Id",
+                keyValue: new Guid("50000000-0000-0000-0000-000000000003"),
+                column: "CreatedAt",
+                value: new DateTime(2025, 11, 20, 14, 0, 15, 517, DateTimeKind.Utc).AddTicks(6878));
+
+            migrationBuilder.UpdateData(
+                table: "Comments",
+                keyColumn: "Id",
+                keyValue: new Guid("50000000-0000-0000-0000-000000000004"),
+                column: "CreatedAt",
+                value: new DateTime(2025, 11, 20, 13, 50, 15, 517, DateTimeKind.Utc).AddTicks(6884));
+
+            migrationBuilder.UpdateData(
+                table: "Comments",
+                keyColumn: "Id",
+                keyValue: new Guid("50000000-0000-0000-0000-000000000005"),
+                column: "CreatedAt",
+                value: new DateTime(2025, 11, 20, 13, 40, 15, 517, DateTimeKind.Utc).AddTicks(6890));
+
+            migrationBuilder.UpdateData(
+                table: "Comments",
+                keyColumn: "Id",
+                keyValue: new Guid("50000000-0000-0000-0000-000000000006"),
+                column: "CreatedAt",
+                value: new DateTime(2025, 11, 20, 13, 30, 15, 517, DateTimeKind.Utc).AddTicks(6897));
+
+            migrationBuilder.UpdateData(
+                table: "Comments",
+                keyColumn: "Id",
+                keyValue: new Guid("50000000-0000-0000-0000-000000000007"),
+                column: "CreatedAt",
+                value: new DateTime(2025, 11, 20, 13, 20, 15, 517, DateTimeKind.Utc).AddTicks(6903));
+
+            migrationBuilder.UpdateData(
+                table: "Comments",
+                keyColumn: "Id",
+                keyValue: new Guid("50000000-0000-0000-0000-000000000008"),
+                column: "CreatedAt",
+                value: new DateTime(2025, 11, 20, 13, 10, 15, 517, DateTimeKind.Utc).AddTicks(6908));
+
+            migrationBuilder.UpdateData(
+                table: "Comments",
+                keyColumn: "Id",
+                keyValue: new Guid("50000000-0000-0000-0000-000000000009"),
+                column: "CreatedAt",
+                value: new DateTime(2025, 11, 20, 13, 0, 15, 517, DateTimeKind.Utc).AddTicks(6967));
+
+            migrationBuilder.UpdateData(
+                table: "Comments",
+                keyColumn: "Id",
+                keyValue: new Guid("50000000-0000-0000-0000-000000000010"),
+                column: "CreatedAt",
+                value: new DateTime(2025, 11, 20, 12, 50, 15, 517, DateTimeKind.Utc).AddTicks(6977));
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: new Guid("40000000-0000-0000-0000-000000000001"),
+                column: "ImageUrl",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: new Guid("40000000-0000-0000-0000-000000000002"),
+                column: "ImageUrl",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: new Guid("40000000-0000-0000-0000-000000000003"),
+                column: "ImageUrl",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: new Guid("40000000-0000-0000-0000-000000000004"),
+                column: "ImageUrl",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: new Guid("40000000-0000-0000-0000-000000000005"),
+                column: "ImageUrl",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: new Guid("40000000-0000-0000-0000-000000000006"),
+                column: "ImageUrl",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: new Guid("40000000-0000-0000-0000-000000000007"),
+                column: "ImageUrl",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: new Guid("40000000-0000-0000-0000-000000000008"),
+                column: "ImageUrl",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: new Guid("40000000-0000-0000-0000-000000000009"),
+                column: "ImageUrl",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Recipes",
+                keyColumn: "Id",
+                keyValue: new Guid("40000000-0000-0000-0000-000000000010"),
+                column: "ImageUrl",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000001"),
+                column: "ConcurrencyStamp",
+                value: "e3b6e3cb-0594-4a19-b076-d52be2071db9");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000002"),
+                column: "ConcurrencyStamp",
+                value: "47306fb7-7a0b-498c-a171-753ec1ea7d07");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000003"),
+                column: "ConcurrencyStamp",
+                value: "47053a59-c38a-49b5-8397-1fd4e60615ba");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000004"),
+                column: "ConcurrencyStamp",
+                value: "1f9d38c3-75fa-4a76-be25-4ed372dcfbff");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000005"),
+                column: "ConcurrencyStamp",
+                value: "d3866fe0-361d-4d64-90b5-e329cc343113");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000006"),
+                column: "ConcurrencyStamp",
+                value: "fb313e47-9b63-473d-89ea-5ad9de979287");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000007"),
+                column: "ConcurrencyStamp",
+                value: "0b110038-3d2e-45c7-a298-011792156ebc");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000008"),
+                column: "ConcurrencyStamp",
+                value: "b8db19e1-1614-4b0b-bf01-95103cc0ec6a");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000009"),
+                column: "ConcurrencyStamp",
+                value: "d6caca66-c863-4adc-99b2-03af11c27a5b");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000010"),
+                column: "ConcurrencyStamp",
+                value: "eaf0b24c-454b-4cc7-973b-52b2b37925a3");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Recipes");
+
+            migrationBuilder.UpdateData(
+                table: "Comments",
+                keyColumn: "Id",
+                keyValue: new Guid("50000000-0000-0000-0000-000000000001"),
+                column: "CreatedAt",
+                value: new DateTime(2025, 11, 12, 11, 16, 8, 730, DateTimeKind.Utc).AddTicks(8641));
+
+            migrationBuilder.UpdateData(
+                table: "Comments",
+                keyColumn: "Id",
+                keyValue: new Guid("50000000-0000-0000-0000-000000000002"),
+                column: "CreatedAt",
+                value: new DateTime(2025, 11, 12, 11, 6, 8, 730, DateTimeKind.Utc).AddTicks(8651));
+
+            migrationBuilder.UpdateData(
+                table: "Comments",
+                keyColumn: "Id",
+                keyValue: new Guid("50000000-0000-0000-0000-000000000003"),
+                column: "CreatedAt",
+                value: new DateTime(2025, 11, 12, 10, 56, 8, 730, DateTimeKind.Utc).AddTicks(8653));
+
+            migrationBuilder.UpdateData(
+                table: "Comments",
+                keyColumn: "Id",
+                keyValue: new Guid("50000000-0000-0000-0000-000000000004"),
+                column: "CreatedAt",
+                value: new DateTime(2025, 11, 12, 10, 46, 8, 730, DateTimeKind.Utc).AddTicks(8655));
+
+            migrationBuilder.UpdateData(
+                table: "Comments",
+                keyColumn: "Id",
+                keyValue: new Guid("50000000-0000-0000-0000-000000000005"),
+                column: "CreatedAt",
+                value: new DateTime(2025, 11, 12, 10, 36, 8, 730, DateTimeKind.Utc).AddTicks(8657));
+
+            migrationBuilder.UpdateData(
+                table: "Comments",
+                keyColumn: "Id",
+                keyValue: new Guid("50000000-0000-0000-0000-000000000006"),
+                column: "CreatedAt",
+                value: new DateTime(2025, 11, 12, 10, 26, 8, 730, DateTimeKind.Utc).AddTicks(8659));
+
+            migrationBuilder.UpdateData(
+                table: "Comments",
+                keyColumn: "Id",
+                keyValue: new Guid("50000000-0000-0000-0000-000000000007"),
+                column: "CreatedAt",
+                value: new DateTime(2025, 11, 12, 10, 16, 8, 730, DateTimeKind.Utc).AddTicks(8661));
+
+            migrationBuilder.UpdateData(
+                table: "Comments",
+                keyColumn: "Id",
+                keyValue: new Guid("50000000-0000-0000-0000-000000000008"),
+                column: "CreatedAt",
+                value: new DateTime(2025, 11, 12, 10, 6, 8, 730, DateTimeKind.Utc).AddTicks(8662));
+
+            migrationBuilder.UpdateData(
+                table: "Comments",
+                keyColumn: "Id",
+                keyValue: new Guid("50000000-0000-0000-0000-000000000009"),
+                column: "CreatedAt",
+                value: new DateTime(2025, 11, 12, 9, 56, 8, 730, DateTimeKind.Utc).AddTicks(8664));
+
+            migrationBuilder.UpdateData(
+                table: "Comments",
+                keyColumn: "Id",
+                keyValue: new Guid("50000000-0000-0000-0000-000000000010"),
+                column: "CreatedAt",
+                value: new DateTime(2025, 11, 12, 9, 46, 8, 730, DateTimeKind.Utc).AddTicks(8667));
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000001"),
+                column: "ConcurrencyStamp",
+                value: "944b6d9b-5858-4443-91dc-f5a5988dcd07");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000002"),
+                column: "ConcurrencyStamp",
+                value: "fabeb2eb-ff4c-49ab-a645-3b869b44ebaa");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000003"),
+                column: "ConcurrencyStamp",
+                value: "1588ce45-cddc-4bbd-9747-499e9e563ed4");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000004"),
+                column: "ConcurrencyStamp",
+                value: "30073aca-572b-4d12-b6a4-cf5eb7b4a319");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000005"),
+                column: "ConcurrencyStamp",
+                value: "d3d7eded-f882-46b6-9250-dd8b30ed0529");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000006"),
+                column: "ConcurrencyStamp",
+                value: "59b9bceb-0e83-48ff-b346-ae0c32e3ec25");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000007"),
+                column: "ConcurrencyStamp",
+                value: "b02ddcdb-f7e9-4488-9ba4-3693da48ccc4");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000008"),
+                column: "ConcurrencyStamp",
+                value: "57cd54cf-4089-4b03-acd6-8a047d84f9c4");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000009"),
+                column: "ConcurrencyStamp",
+                value: "46f3e402-93f3-45e0-9d6e-f6826ca338d4");
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: new Guid("00000000-0000-0000-0000-000000000010"),
+                column: "ConcurrencyStamp",
+                value: "dfe6ad1a-ccf3-42b1-a723-5530bd8d5514");
+        }
+    }
+}
